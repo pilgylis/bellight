663cf2c90ecaa664d71295209e54b5b076a10a2a
Nguyễn Việt Trung <nguyen.viettrung175@gmail.com>
2022-09-06 10:35:29 +0700

    publish data management

---
+++ src/Bellight.MongoDb/publish.ps1
@@ -0,0 +1,4 @@
+$ver = $args[0]
+$key = $args[1]
+dotnet build -c "Release" -p:Version=$ver
+dotnet nuget push bin\Release\Bellight.AutoMapper.$ver.nupkg -k $key -s https://api.nuget.org/v3/index.json
